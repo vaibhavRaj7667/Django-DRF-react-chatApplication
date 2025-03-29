8412707fa0f022339cc4741f7c04fb5c6401f386
vaibhavRaj7667 <zivaibhav1@gmail.com>
2025-03-29 14:42:21 +0530

    fixed frontend

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -1,15 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import '../stylesheet/chatPage.css';
 import { Refresh } from './RefreshToken';
+import axios from 'axios';
 
 const ChatPage = () => {
-  const [persons, setPersons] = useState([
-    { Name: "Kelly Tran", message: "This is a test message", pic: "https://i.pinimg.com/736x/b6/d5/2e/b6d52e87bf3fb1f9c26672ad8e0370c3.jpg" },
-    { Name: "Kelly Tran", message: "This is a test message", pic: "https://i.pinimg.com/736x/b6/d5/2e/b6d52e87bf3fb1f9c26672ad8e0370c3.jpg" },
-    { Name: "Kelly Tran", message: "This is a test message", pic: "https://i.pinimg.com/736x/b6/d5/2e/b6d52e87bf3fb1f9c26672ad8e0370c3.jpg" },
-    { Name: "Kelly Tran", message: "This is a test message", pic: "https://i.pinimg.com/736x/b6/d5/2e/b6d52e87bf3fb1f9c26672ad8e0370c3.jpg" }
-  ]);
+  const [Values, setValues]=useState([])
+  
 
+  useEffect(()=>{
+
+    const fetchdata= async()=>{
+      const token = localStorage.getItem("access_token")
+      let response = await axios.get("http://127.0.0.1:8000/friends/1/",{
+        headers: {
+          Authorization: `Bearer ${token}` 
+        }
+      })
+    
+      setValues(response.data)
+      
+    }
+    fetchdata() 
+
+  },[])
+
+  useEffect(() => {
+    console.log("Updated Values:", Values);
+  }, [Values]); 
+
+  
   const [messages, setMessages] = useState([
     { incoming: true, text: "Hi there" },
     { incoming: false, text: "I am fine, how about you?" }
@@ -33,12 +52,12 @@ const ChatPage = () => {
       <div className='peoples'>
         <input type="text" placeholder='Search...' />
 
-        {persons.map((data, index) => (
+        {Values.map((data, index) => (
           <div key={index} className='data'>
-            <img src={data.pic} alt="" />
+            <img src="https://i.pinimg.com/736x/b6/d5/2e/b6d52e87bf3fb1f9c26672ad8e0370c3.jpg" alt="" />
             <div className='text_message'>
-              <h4>{data.Name}</h4>
-              <p>{data.message}</p>
+              <h4>{data.username}</h4>
+              <p>This is a test message</p>
             </div>
           </div>
         ))}
